8bd94de0259e4a9a187c17bc60443dd5262509aa
Kyle Parry <kyle.parry1@pm.me>
2024-08-08 20:57:41 +0100

    Initial commit

---
+++ src/main/java/com/kyleparry/web/crawler/api/WebCrawlerApiApplication.java
@@ -0,0 +1,13 @@
+package com.kyleparry.web.crawler.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebCrawlerApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebCrawlerApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/kyleparry/web/crawler/api/WebCrawlerApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kyleparry.web.crawler.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebCrawlerApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
